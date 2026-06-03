33e187ce2de37e6ada8e7141e4e541e226da975f
JesusUc18 <jesus.uc@tecdesoftware.edu.mx>
2026-06-02 23:41:24 -0600

    CREATE: add Paciente, Medico and Cita entities

---
+++ Models/Medico.cs
@@ -0,0 +1,11 @@
+﻿namespace CitasApp.Models
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Apellido { get; set; }
+        public string Especialidad { get; set; }
+        public string NumeroLicencia { get; set; }
+    }
+}
